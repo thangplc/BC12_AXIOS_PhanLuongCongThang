c45823ae7ca81865e72b074ad527a982d2d1a00e
thangplc <thangplc1412@gmail.com>
2021-07-19 17:36:02 +0700

    fix bug

--- Tabula/app/services/UserService.js
+++ Tabula/app/services/UserService.js
@@ -1,7 +1,7 @@
 function UserService() {
     this.getData = function() {
         return axios({
-            url: `https://60f3ffaf3cb0870017a8a0cf.mockapi.io/Users`,
+            url: `https://60f3ffaf3cb0870017a8a0cf.mockapi.io/Members/`,
             method: 'GET',
         });
     };
